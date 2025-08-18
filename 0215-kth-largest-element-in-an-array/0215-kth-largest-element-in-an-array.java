b57e2dcfcd34010a9d4cf50c60783302f3643633
Dip Pansuriya <pansuriy@usc.edu>
2025-08-17 18:47:30 -0700

    Time: 57 ms (62.64%), Space: 58.6 MB (48.54%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,15 +1,64 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b - a);
 
         for(int num : nums) {
-            minHeap.add(num);
+            maxHeap.add(num);
         }
 
-        while(!minHeap.isEmpty() && minHeap.size() > k) {
-            minHeap.poll();
+        while(k > 1) {
+            maxHeap.poll();
+            k--;
         }
 
-        return minHeap.peek();
+        return maxHeap.peek();
     }
 }
+
+/**
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length -1, nums.length - k);
+    }
+
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if(left == right) {
+            return nums[left];
+        }
+
+        int pIdx = partition(nums, left, right);
+
+        if(pIdx == k) {
+            return nums[k];
+        } else if (pIdx < k) {
+            return quickSelect(nums, pIdx + 1, right, k);
+        } else {
+            return quickSelect(nums, left, pIdx - 1, k);
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int s_idx = left;
+        int pivot = nums[right];
+
+        for(int i=left; i <= right; i++) {
+            if(nums[i] < pivot) {
+                swap(nums, s_idx, i);
+                s_idx++;
+            }
+        }
+
+        swap(nums, s_idx, right);
+
+        return s_idx;
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
+
+ */
